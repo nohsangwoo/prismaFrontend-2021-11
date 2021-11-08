056f211517599d35bb82ba436254e595478c69fc
nohsangwoo <nsgr12@naver.com>
2021-11-08 18:45:45 +0900

    test logged in activate

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import userSlice from 'store/reducers/userSlice';
 const Home = (): JSX.Element => {
-    return <div>Home</div>;
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={() => dispatch(userSlice.actions.setLoggedOut())}>
+                Log Out!
+            </button>
+        </div>
+    );
 };
 
 export default Home;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import userSlice from 'store/reducers/userSlice';
 const Login = (): JSX.Element => {
-    return <div>Login</div>;
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <h1>Login</h1>
+            <button onClick={() => dispatch(userSlice.actions.setLoggedIn())}>
+                Log in now!
+            </button>
+        </div>
+    );
 };
 
 export default Login;
