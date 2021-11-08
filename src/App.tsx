bd31607f360fda721036600dc8bef9be8bca3346
nohsangwoo <nsgr12@naver.com>
2021-11-08 17:06:12 +0900

    setting react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-    return <div>haha</div>;
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
