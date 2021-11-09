6b5f3830a37986ebfe604f587d1affba3c74c807
nohsangwoo <nsgr12@naver.com>
2021-11-09 14:37:52 +0900

    add comment to global theme

--- src/styles/styled.d.tsx
+++ src/styles/styled.d.tsx
@@ -2,6 +2,7 @@
 import 'styled-components';
 // 여기 styled theme - typescript 적용
 
+// styled-components안에 있는 DefaultTheme interface를 overwriting 하는 작업이다.
 declare module 'styled-components' {
     // DefaultTheme에 대한 interface를 적어두면 DefaultTheme사용시 type정책 사용가능
     export interface DefaultTheme {
